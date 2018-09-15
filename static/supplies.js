4c9ee06a52c836e6a6a76c96327510b829662c24
Andres Rios <rianby64@gmail.com>
2018-09-15 13:16:37 +0300

    Defina las columnas para las cantidades y valor parcial

--- static/supplies.js
+++ static/supplies.js
@@ -6,10 +6,26 @@
     'description',
     'cost',
     'type',
-    'unit'
+    {
+      key: 'Quantity',
+      call: (selection) => {
+        selection.text(d => {
+          return (Number(d.qop || 0) * Number(d.quantity || 0)).toFixed(2);
+        });
+      }
+    },
+    'unit',
+    {
+      key: 'PartialCost',
+      call: (selection) => {
+        selection.text(d => {
+          return Number((Number(d.cost || 0) * Number(d.qop || 0) * Number(d.quantity || 0)).toFixed(0)).toLocaleString();
+        });
+      }
+    }
   ];
 
-  const header = ['APU', 'Insumo', 'Descripcion', 'Costo', 'Tipo', 'Unidad'];
+  const header = ['APU', 'Insumo', 'Descripcion', 'Costo', 'Tipo', 'Cantidad', 'Unidad', 'Valor Parcial'];
 
   window.supplies = setupTable({
     module: 'Supplies',
